2b96675ddc353fb4b313866a9472eb59d6d87d91
jullian972 <94387110+jullian972@users.noreply.github.com>
2024-06-30 15:50:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleDetails(id) {
   var details = document.getElementById(id);
-  if (details.style.display === "none") {
+  if (details.style.display === "none" || details.style.display === "") {
     details.style.display = "block";
   } else {
     details.style.display = "none";
